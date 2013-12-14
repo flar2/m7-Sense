c5d5db63564db88af9fe3ab5f9990311fe04ba0e
flar2 <asegaert@gmail.com>
2013-12-13 23:18:24 -0500

    allow moc-crypto modules to load
    Signed-off-by: flar2 <asegaert@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -1029,7 +1029,8 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
-	
+	if(!strncmp("moc_", mod->name, 4)) return 1;
+
 	if (!crc)
 		return 1;
 
